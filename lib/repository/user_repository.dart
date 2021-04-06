5b07f56ea1d322c4267d73f2b076be07e95b1d74
Aryan Agarwal <aryankush025@gmail.com>
2021-04-06 13:17:18 +0530

    https code refactoring

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -4,51 +4,29 @@ import 'package:http/http.dart' as http;
 
 import '../models/user.dart';
 
-const String base_url = 'https://lets-chat-server-app.herokuapp.com';
+const String base_url = 'lets-chat-server-app.herokuapp.com';
 
 class UserRepository {
   Future<User> logIn({
     @required String email,
     @required String password,
   }) async {
-    // final body = json.encode({
-    //   'email': email,
-    //   'password': password,
-    // });
-
-    // final response = await http.post(
-    //   Uri.https(base_url, 'login'),
-    //   body: body,
-    // );
-
-    // if (response.statusCode == 200) {
-    //   return User.fromJson(
-    //     json.decode(
-    //       response.body,
-    //     ),
-    //   );
-    // } else {
-    //   throw Exception('Failed to load user');
-    // }
-
     const headers = {'Content-Type': 'application/json'};
-    var request = http.Request('POST', Uri.parse('$base_url/login'));
-
-    request.body = json.encode({
+    final body = json.encode({
       'email': email,
       'password': password,
     });
 
-    request.headers.addAll(headers);
-
-    http.StreamedResponse response = await request.send();
-
-    String responseString = await response.stream.bytesToString();
+    final response = await http.post(
+      Uri.https(base_url, 'login'),
+      body: body,
+      headers: headers,
+    );
 
     if (response.statusCode == 200) {
       return User.fromJson(
         json.decode(
-          responseString,
+          response.body,
         ),
       );
     } else {
